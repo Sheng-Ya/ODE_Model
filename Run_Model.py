a408349f6eb2e6c2025aa32f058cc5456d5945b7
Sheng-Ya <sw4924@ic.ac.uk>
2025-06-04 11:15:07 +0100

    check results for previous version

--- Run_Model.py
+++ Run_Model.py
@@ -99,6 +99,7 @@ def combined_system(t, Initial_Conditions_numpy, Parameters, Initial_Conditions_
     # d_resp_mech = respiratory_mechanics(t, resp_mech_state, Parameters, Initial_Conditions_dict, num_removed, i)
 
     d_combined = np.concatenate((d_cardio, d_cardio_contr, d_gas, d_resp_vent))
+    A = list(d_combined)
 
     # if np.any(np.isnan(d_combined)) or np.any(np.isinf(d_combined)):
     #     print(f"NaN or Inf detected at t = {t}")
