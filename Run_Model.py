2b4e122646c50d26fcece252f97e8f389801403b
Sheng-Ya <sw4924@ic.ac.uk>
2025-06-05 20:52:31 +0100

    edit

--- Run_Model.py
+++ Run_Model.py
@@ -74,7 +74,7 @@ def combined_system(t, Initial_Conditions_numpy, Parameters, Initial_Conditions_
 
     d_combined = np.concatenate((d_cardio, d_cardio_contr, d_gas, d_resp_vent))
     A = list(d_combined)
-    if t > 0.8579:
+    if t > 0.857:
         Aaa = 2
 
     # if np.any(np.isnan(d_combined)) or np.any(np.isinf(d_combined)):
