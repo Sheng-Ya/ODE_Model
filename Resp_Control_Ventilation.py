0d1a3b5ab15886e15873f8ba86e2ecdda5ef36d3
Sheng-Ya <sw4924@ic.ac.uk>
2025-06-06 11:17:59 +0100

    edit

--- Resp_Control_Ventilation.py
+++ Resp_Control_Ventilation.py
@@ -49,7 +49,7 @@ def resp_control_vent(t, state, params, updates, gas_exchange_inputs, num_remove
     Pa_CO2_history = updates["Pa_CO2_history"]
     Pb_CO2_history = updates["Pb_CO2_history"]
 
-    last_breath_time = max(0, t - updates["finish_breath_time"][-1])
+    last_breath_time = max(0, (t - updates["finish_breath_time"][-1]))
 
     resp_cycle = last_breath_time % (t1 + t2)
     if t <= (t1 + t2) and updates["finish_breath_time"][-1] == 0:
