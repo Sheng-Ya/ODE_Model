c32871a91a05a04ee68572e9786d3f1f65320ea6
Sheng-Ya <sw4924@ic.ac.uk>
2025-06-06 19:13:53 +0100

    edit Ta > 0

--- Gas_Exchange.py
+++ Gas_Exchange.py
@@ -94,7 +94,7 @@ def gas_exchange(t, state, params, time_history, resp_control_inputs, heart_syst
 
     t_minus_Ta = t - Ta
 
-    if t_minus_Ta >= t_start and t > abs(Ta):
+    if t_minus_Ta >= t_start and t > abs(Ta) and Ta > 0:
         # Find the index for delay_time in time_history
         delay_index = bisect.bisect_right(time_history, t_minus_Ta) - 1
         PA_O2_old = updates["PA_O2"][delay_index]
