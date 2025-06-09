5f3fe54c18a22240ef397bad089e0e1bb5135ff5
Sheng-Ya <sw4924@ic.ac.uk>
2025-06-09 12:31:33 +0100

    edit

--- Run_Model.py
+++ Run_Model.py
@@ -119,7 +119,7 @@ def combined_system(t, Initial_Conditions_numpy, Parameters, Initial_Conditions_
     A = list(d_combined)
     # if np.any(np.isnan(d_combined)) or np.any(np.isinf(d_combined)):
     #     print(f"NaN or Inf detected at t = {t}")
-    if t> 10:
+    if t> 6.2:
         AAA = 2
 
     if num_removed == 0:
