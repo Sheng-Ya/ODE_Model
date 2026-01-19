0f9376174b7cedacc8b0f9a159b955ce7eaff9d0
Sheng-Ya <sw4924@ic.ac.uk>
2026-01-19 11:03:01 +0000

    Just name P without Pmax

--- All_derivatives.py
+++ All_derivatives.py
@@ -606,7 +606,7 @@ def njit_compatible(t, state, num_removed, i, BUFFER_LIMIT, all_time, Input_Para
     Qi_lv = valve_signal * (np.sqrt(np.maximum(P_la - P_lv, 0)) * AR_mi * R_mi)
 
     # Dynamics with smooth transition
-    d2theta_mi_dt2 = valve_signal * ((Pmax_la - P_lv) * Kp_mi * np.cos(theta_mi) - Kf_mi * dtheta_mi_dt +
+    d2theta_mi_dt2 = valve_signal * ((P_la - P_lv) * Kp_mi * np.cos(theta_mi) - Kf_mi * dtheta_mi_dt +
             Kb_mi * Qi_lv * np.cos(theta_mi) - Kv_mi * Qi_lv * np.sin(2 * theta_mi))
 
     P_la = Pmax_la
