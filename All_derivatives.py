b775cf2925b65a5baff82662cd04cc98863c19e7
Sheng-Ya <sw4924@ic.ac.uk>
2026-01-19 11:02:23 +0000

    Just name P without Pmax

--- All_derivatives.py
+++ All_derivatives.py
@@ -577,7 +577,6 @@ def njit_compatible(t, state, num_removed, i, BUFFER_LIMIT, all_time, Input_Para
     #
     #     d2theta_mi_dt2 = (P_la - P_lv) * Kp_mi * np.cos(theta_mi) - Kf_mi * dtheta_mi_dt + Kb_mi * Qi_lv * np.cos(
     #         theta_mi) - Kv_mi * Qi_lv * np.sin(2*theta_mi)
-    #     P_la = Pmax_la
     # else:
     #     Qi_lv = 0
     #     AR_mi = 0
